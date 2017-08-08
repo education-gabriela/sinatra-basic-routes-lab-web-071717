ccdac70cbbb1c589f2762382b472c5380e6ee120
gabidavila <gabidavila@gmail.com>
2017-08-07 21:04:16 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/name" do
+    "My name is Gabi"
+  end
+  get "/hometown" do
+    "My hometown is Vitória"
+  end
+  get "/favorite-song" do
+    "My favorite song is Bad Reputation"
+  end
+  get "/" do
+    "Hello, World!"
+  end
 end
+
